2ef6c4bc686183c065babe574fa4fad60beb497c
AndreyBaglai <andreybaglai@gmail.com>
2021-10-08 11:20:22 +0300

    init: add basic structure files

---
+++ src/containers/App/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Layout } from 'antd';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Layout>
+        {/* <Header>Header</Header>
+        <Layout>
+          <Sider>Sider</Sider>
+          <Content>Content</Content>
+        </Layout>
+        <Footer>Footer</Footer> */}
+      </Layout>
+    </div>
+  );
+};
+
+export default App;
