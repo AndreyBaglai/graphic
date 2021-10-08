b90dbc4c5083dbf5ca1549c2f6c79ef4d7c1147d
AndreyBaglai <andreybaglai@gmail.com>
2021-10-08 15:07:33 +0300

    feat: top content for main

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
-import { Content } from 'antd/lib/layout/layout';
+import { Button, Typography } from 'antd';
 
-//import styles from './styles.module.scss';
+import styles from './styles.module.scss';
 
 const Main: React.FC = () => {
   return (
-    <Content
-      className="site-layout-background"
-      style={{
-        padding: 24,
-        margin: 0,
-        minHeight: 280,
-      }}>
-      Content
-    </Content>
+    <main className={styles.main}>
+      <div className={styles.top}>
+        <Typography.Title level={2} className={styles.title}>
+          Optiona1
+        </Typography.Title>
+        <div className={styles.buttonsWrapper}>
+          <Button type="primary" size="large">Coin</Button>
+          <Button type="primary" size="large">Line</Button>
+        </div>
+        <ul className={styles.period}>
+          <li>3d</li>
+          <li>7d</li>
+          <li>2w</li>
+          <li>1m</li>
+        </ul>
+      </div>
+    </main>
   );
 };
 
